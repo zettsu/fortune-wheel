8d32b36e3c8c628b1902ad019ad777672cb2f59e
Matias Olivera <jmatias.olivera@gmail.com>
2022-03-20 23:43:18 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <Roulette ref="wheel" :items="items" @click="launchWheel" />
+</template>
+
+<script>
+import { Roulette } from "vue3-roulette";
+export default {
+  name: "App",
+  components: {
+    Roulette,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          name: "Banana",
+          htmlContent: "Banana",
+          textColor: "",
+          background: "",
+        },
+        {
+          id: 2,
+          name: "Apple",
+          htmlContent: "Apple",
+          textColor: "",
+          background: "",
+        },
+        {
+          id: 3,
+          name: "Orange and Purple",
+          htmlContent: "Orange<br>and Purple",
+          textColor: "",
+          background: "",
+        },
+        {
+          id: 4,
+          name: "Cherry",
+          htmlContent: "Cherry",
+          textColor: "",
+          background: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    launchWheel() {
+      this.$refs.wheel.launchWheel();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
